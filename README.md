aa8abd4e67c193a407151d113e7b390b071a6fc9
ymingsu <50993732+ymingsu@users.noreply.github.com>
2024-06-08 20:06:05 +0800

    Update parameters

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # UDPLC
 Source code for paper: Underwater Descattering with Polarization Line Constraint.
 
-run `main.m` on R2022a.
+run `main.m` on R2022a or newer version.
+
+If the paper is useful to you, please cite it:
+
+```
+@Article{Su2024Underwater,
+  author   = {Yiming Su and Zhen Zhang and Yang Lu and Shiben Liu and Yandong Tang and Jiandong Tian},
+  journal  = {Optics and Lasers in Engineering},
+  title    = {Underwater descattering with polarization line constraint},
+  year     = {2024},
+  issn     = {0143-8166},
+  pages    = {108265},
+  volume   = {179},
+  doi      = {https://doi.org/10.1016/j.optlaseng.2024.108265},
+  keywords = {Underwater descattering, Polarization imaging model, Polarization quantization, Polarization line constraint},
+  url      = {https://www.sciencedirect.com/science/article/pii/S0143816624002446},
+}
+```
--- main.m
+++ main.m
@@ -8,6 +8,20 @@ load('./temp-09082021111910.mat');
 [J_dehaze,t_est,r_est,mD_est,mB_est,B_inf] = PLC(loadData);
 % The latter five outputs are varargout.
 % varargout=t_est,r_est,mD_est,mB_est,B_inf
+% one can modified the parameters to see their effect.
+% default_paras=struct(... % the default parameters for plc
+% 'n_points',500,... % the polarization line number are n_points*3
+% 'lambda',0.1, ... % see wls lambda
+% 'B_inf',[],... % user input of Backscater light at infinity 
+% 'mB',[],... % user input of Backscater polarization state
+% 'B_inf_method','quadTree',... % Backscater estimation method. i.e. quadTree, DCP
+% 'eta',1.15,... % parameter for calculating mB
+% 'bBoostEnabled',true,... % Resize the image smaller to speed it up
+% 'alpha',0.95,... % parameter for calculating td
+% 'BoostScale',1/8,... % parameter for imresize
+% 'wbmethod','gray'... % white balance method, i.e. gray, pca, white or off.
+% );
+% [J_dehaze,t_est,r_est,mD_est,mB_est,B_inf] = PLC(loadData,default_paras);
 %% Restored Polarization information
 % J_DoLP=abs(mD_est);
 % J_AoLP=1/2*mod(angle(mD_est),2*pi);
